b7db35668a354b79c917b668381473d969d1ef25
Nikola <dzonza996@gmail.com>
2023-11-08 18:43:14 +0100

    Upadting django settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3o4%8-#f4c%3hi61qb-farnlro5bg1p+aj1@ybd(azfxw#l+2'
+SECRET_KEY = os.environ.get('SECRET_KET', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
